26aea5058fc6b084973e2d9e4bd78a7a76610f39
Vadim Makishvili <makishvili@yandex-team.ru>
2013-03-26 16:25:44 +0200

    Два последовательных jaggi блока

--- desktop.bundles/list/list.blocks.js
+++ desktop.bundles/list/list.blocks.js
@@ -9,35 +9,40 @@
 */
 module.exports = {
     call : {
-        'getHtml' : {
+        'rawHtml' : {
             call: 'file',
             params: {
-                path: '/Users/makishvili/projects/storymill-export/desktop.bundles/list/king.html',
-                toState : { rawHtml : '.[0]' }
-            }
-        },
-        'html2json' : {
-            deps: 'getHtml',
-            guard: 'rawHtml',
-            params : function(ctx) {
-                return { rawHtml : ctx.state('rawHtml') };
+                path: '/Users/makishvili/projects/storymill-export/desktop.bundles/list/king.html'
             },
-            call: 'includes/html2json.js'
+            toState : { htmlString : '.[0]' }
+        },
+        'rawJson' : {
+            deps: 'rawHtml',
+            guard: 'htmlString',
+            call: 'includes/rawJson.js',
+            params : function(ctx) {
+                return { htmlString : ctx.state('htmlString') };
+            },
+            pointer: '.[0].children[1].children'
         }
     },
 
     done : function(res, _, promise) {
-        promise.fulfill(getStoryJSON(res.html2json));
+        promise.fulfill(getStoryJSON(res.rawJson));
     }
 };
 
-function getStoryJSON(stories) {
-    return stories ?
-        stories.map(function(para) {
+function getStoryJSON(parasArr) {
+    var bemjson = [];
+
+    if (parasArr) {
+        bemJson = parasArr.map(function(para) {
             return {
                 tag : para.attribs.class,
                 text: para.children[0].data
             };
-        }) :
-        [];
+        })
+    }
+
+    return bemjson;
 }
